cdc360bbf0d62cb7868d9899e1a2975ed4d7390f
Brandon Knox <bknox.dev@gmail.com>
2021-05-20 16:13:17 -0700

    example of swift not knowing how to sort struct (could be first name, last name, both, etc)

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,12 +7,22 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+}
+
 struct ContentView: View {
-    let values = [1, 5, 3, 6, 2, 9].sorted()
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister")
+    ].sorted()
     
     var body: some View {
-        List(values, id: \.self) {
-            Text(String($0))
+        List(users) { user in
+            Text("\(user.lastName), \(user.firstName)")
         }
     }
 }
