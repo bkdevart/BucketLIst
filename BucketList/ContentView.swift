44da3d2283d5f32cde6d3ca3947fb50090bd499d
Brandon Knox <bknox.dev@gmail.com>
2021-05-21 11:50:45 -0700

    skeleton code for adding authentication

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,13 +5,34 @@
 //  Created by Brandon Knox on 5/20/21.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
-
+    @State private var isUnlocked = false
+    
     var body: some View {
-        MapView()
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        Text("Hello world")
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in DispatchQueue.main.async {
+                    if success {
+                        // authenticated successfully
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
     }
 }
 
