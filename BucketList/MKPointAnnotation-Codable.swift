12e6c2509fae367cb7a3c1955138084f8ce6647a
Brandon Knox <bknox.dev@gmail.com>
2021-05-24 11:32:55 -0700

    Example of extending a class to conform to Codable that does not work.  Because MKPointAnnotation has subclasses, there is no way we can guarantee they also conform to Codable

---
+++ BucketList/MKPointAnnotation-Codable.swift
@@ -0,0 +1,19 @@
+//
+//  MKPointAnnotation-Codable.swift
+//  BucketList
+//
+//  Created by Brandon Knox on 5/24/21.
+//
+
+import Foundation
+import MapKit
+
+extension MKPointAnnotation: Codable {
+    public required init(from decoder: Decoder) throws {
+        
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        
+    }
+}
