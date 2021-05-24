2219512962b2b05f594f68753bee30219be20180
Brandon Knox <bknox.dev@gmail.com>
2021-05-23 20:55:54 -0700

    Extended MKPointAnnotation to conform to Observable object.  Created EditView for adding information about dropped pins.  Modified ContentView to display EditView.

---
+++ BucketList/EditView.swift
@@ -0,0 +1,34 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Brandon Knox on 5/23/21.
+//
+
+import MapKit
+import SwiftUI
+
+struct EditView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var placemark: MKPointAnnotation
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Place name", text: $placemark.wrappedTitle)
+                    TextField("Description", text: $placemark.wrappedSubtitle)
+                }
+            }
+            .navigationBarItems(trailing: Button("Done") {
+                                    self.presentationMode.wrappedValue.dismiss()
+            })
+        }
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView(placemark: MKPointAnnotation.example)
+    }
+}
